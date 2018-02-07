8719f969c7690fc7d48d74116f38373ed63cc71c
Nova360 <anthonynewman40.pn@gmail.com>
2018-02-07 12:02:05 -0500

    The base version

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,25 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  answers : any;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController) 
+  {
 
   }
 
+ public display( val: any) : void
+  {
+    this.answers = document.getElementById('ans').innerHTML = document.getElementById('ans').innerHTML + val;
+  //  console.log(val);
+   //console.log(eval(this.answers));
+  }
+  public  allClear()
+  {
+    document.getElementById('ans').innerHTML = "";
+  }
+  public equal()
+  {
+    document.getElementById('ans').innerHTML =  eval(this.answers).toFixed(2);
+  }
 }
